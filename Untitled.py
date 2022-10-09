82b76419798f830cb78ab05a511249f27799f54b
Kamal Rhrabla <kamalghrabla@gmail.com>
2022-10-09 18:37:06 +0100

    jupy pandas conda

---
+++ Untitled.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import pandas as pd
+
+
+# In[2]:
+
+
+df = pd.read_csv('dataset-sell4all-maroc.csv')
+
+
+# In[3]:
+
+
+df.head()
+
+
+# In[ ]:
+
+
+
+
