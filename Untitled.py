463d6e3a34283e6ad31f0e01d944395bb992f16d
Kamal Rhrabla <kamalghrabla@gmail.com>
2022-10-09 22:36:33 +0100

    edit cleaner file

--- Untitled.py
+++ Untitled.py
@@ -5,18 +5,53 @@
 
 
 import pandas as pd
-
-
-# In[2]:
-
+import matplotlib.pyplot as plt
+import numpy as np
+import seaborn as sns 
+from IPython.display import display
 
 df = pd.read_csv('dataset-sell4all-maroc.csv')
 
-
-# In[3]:
-
-
 df.head()
+
+df.count()
+
+pd.options.display.max_columns = None
+
+display(df)
+
+df.dtypes
+
+df['Age'].mean()
+
+df['Customer spendings'].mean()
+
+
+# median MPG for each Company
+df.groupby('Country')['Age'].median()
+
+
+sns.barplot(x=df['Country'].head(3),y=df['Customer spendings'],data=df)
+
+
+# In[22]:
+
+
+sns.barplot(x=df['Country'].head(36),y=df['Customer spendings'],data=df)
+
+
+# In[23]:
+
+
+
+df.drop(df[df['Customer spendings'] < 10].index, inplace = True)
+
+df.drop_duplicates()
+
+df.drop(columns=['Email', 'Phone Number','Name','Address','Postal code','Last date of connection','Last time of connection'])
+
+
+df.to_csv('cleaned_data.csv')
 
 
 # In[ ]:
